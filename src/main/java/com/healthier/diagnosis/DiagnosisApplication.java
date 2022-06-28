54cc25bb4c12b0937b74a93fe25fa5bc074a97d9
offsujin <offsujin@gmail.com>
2022-06-28 22:14:48 +0900

    Init

---
+++ src/main/java/com/healthier/diagnosis/DiagnosisApplication.java
@@ -0,0 +1,13 @@
+package com.healthier.diagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthier/diagnosis/DiagnosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthier.diagnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
