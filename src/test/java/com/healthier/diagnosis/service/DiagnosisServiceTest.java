148b695d50569a7c6f3c6e705ddd7f6bc2e8c326
offsujin <offsujin@gmail.com>
2022-07-09 17:24:53 +0900

    Fix : Test template 적용
    Test template 적용

--- src/test/java/com/healthier/diagnosis/service/DiagnosisServiceTest.java
+++ src/test/java/com/healthier/diagnosis/service/DiagnosisServiceTest.java
@@ -23,11 +23,14 @@ class DiagnosisServiceTest {
     @DisplayName("진단 결과 조회")
     @Test
     void findDiagnosis(){
+        //given
         String id = "62c795799d8ed7017f145df8";
 
+        //when
         DiagnosisResponseDto diagnosis = diagnosisService.findDiagnosis(id);
 
-        assertThat(diagnosis.getDiagnostic_result().getSeverity()).isEqualTo(2);
-        assertThat(diagnosis.getDiagnostic_result().getH1()).isEqualTo("나도 모르는 새에 자꾸자꾸 깨는 당신은");
+        //then
+        assertThat(diagnosis.getDiagnosticResult().getSeverity()).isEqualTo(2);
+        assertThat(diagnosis.getDiagnosticResult().getH1()).isEqualTo("나도 모르는 새에 자꾸자꾸 깨는 당신은");
     }
 }
