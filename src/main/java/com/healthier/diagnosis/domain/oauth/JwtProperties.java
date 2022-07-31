474f02a93c37c1289708810c71c858bfed2bec0d
zinc <your_zinc@ewhain.net>
2022-07-31 22:54:40 +0900

    feat : SECRET_KEY update

--- src/main/java/com/healthier/diagnosis/domain/oauth/JwtProperties.java
+++ src/main/java/com/healthier/diagnosis/domain/oauth/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.healthier.diagnosis.domain.oauth;
 
 public interface JwtProperties {
-    String SECRET = "{}";
+    String SECRET = "${SECRET}"; //"wearestrong";
     Long EXPIRED_TIME = 1000 * 60L * 60L * 1L;  // 2시간
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
