54248ce7d552803e42a0a0f093e439be54fb661b
zinc <your_zinc@ewhain.net>
2022-07-31 22:40:40 +0900

    fix : cors error

--- src/main/java/com/healthier/diagnosis/controller/UserController.java
+++ src/main/java/com/healthier/diagnosis/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping(value="/api")
 @RestController
