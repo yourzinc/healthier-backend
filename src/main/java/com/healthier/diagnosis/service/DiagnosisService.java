3ad245fdb517397fc3048129a3d5b635a8dcf6e7
yourzinc <your_zinc@ewhain.net>
2022-09-17 04:38:18 +0900

    feat: (두통) 약물과용두통/경미/주의/심각/ 진단 수정

--- src/main/java/com/healthier/diagnosis/service/DiagnosisService.java
+++ src/main/java/com/healthier/diagnosis/service/DiagnosisService.java
@@ -81,37 +81,27 @@ public class DiagnosisService {
     /**
      * 두통 : 약물과용 두통, 경미, 주의, 심각 두통 진단
      */
-    public DiagnosisResponseDto checkMOH_mild_warning_severe(String id,
-                                                             int is_taking_medicine,
-                                                             int pain_level,
-                                                             int period,
-                                                             int cycle)
+    public String checkMOH_mild_warning_severe(String id,
+                                               int is_taking_medicine,
+                                               int pain_level,
+                                               int period,
+                                               int cycle)
     {
         Diagnosis diagnosis = diagnosisRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.DIAGNOSIS_NOT_FOUND));
 
-        Diagnosis resultDiagnosis = null;
-
-        if (is_taking_medicine == 1) { // 약물과용 두통
-            resultDiagnosis = diagnosisRepository.findById(diagnosis.getMOH())
-                    .orElseThrow(() -> new CustomException(ErrorCode.DIAGNOSIS_NOT_FOUND));
+        if (is_taking_medicine == 0) { // 약물과용 두통
+            return diagnosis.getMOH();
         }
-        else if ((period == 2) | (period == 3) & (cycle == 1)) { // 만성
-            resultDiagnosis = diagnosisRepository.findById(diagnosis.getSevere_headache())
-                    .orElseThrow(()-> new CustomException(ErrorCode.DIAGNOSIS_NOT_FOUND));
+        else if ( ((period == 2) | (period == 3)) & (cycle == 0)) { // 만성
+            return diagnosis.getSevere_headache();
         }
         else if ((pain_level == 1 ) | (pain_level == 2)) { // 경미
-            resultDiagnosis = diagnosisRepository.findById(diagnosis.getMild_headache())
-                    .orElseThrow(()-> new CustomException(ErrorCode.DIAGNOSIS_NOT_FOUND));
+            return diagnosis.getMild_headache();
         }
-        else if ((pain_level == 3 ) | (pain_level == 4)) { // 주의
-            resultDiagnosis = diagnosisRepository.findById(diagnosis.getWarning_headache())
-                    .orElseThrow(()-> new CustomException(ErrorCode.DIAGNOSIS_NOT_FOUND));
+        else { // 주의
+            return diagnosis.getWarning_headache();
         }
 
-        return DiagnosisResponseDto.builder()
-                .isResult(1)
-                .diagnosticResult(resultDiagnosis)
-                .build();
     }
 }
