9b994afefec093e75b2d2a870750f5071f4ec670
yourzinc <your_zinc@ewhain.net>
2022-09-17 02:55:00 +0900

    docs: API 주석 추가

--- src/main/java/com/healthier/diagnosis/controller/QuestionController.java
+++ src/main/java/com/healthier/diagnosis/controller/QuestionController.java
@@ -16,6 +16,7 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    // 진단 질문 응답
     @PostMapping
     public ResponseEntity<?> getNextQuestion(@RequestBody @Valid QuestionRequestDto dto) {
         return ResponseEntity.ok(questionService.findNextQuestion(dto));
@@ -27,6 +28,7 @@ public class QuestionController {
         return ResponseEntity.ok(questionService.findFirstQuestion("sleepdisorder"));
     }
 
+    // 수면장애 결정적 진단 응답
     @PostMapping (value = "/sleepdisorder/decisive")
     public ResponseEntity<?> getDecisiveQuestion(@RequestBody @Valid DecisiveQuestionRequestDto dto) {
         return ResponseEntity.ok(questionService.findDecisiveQuestion(dto));
@@ -38,12 +40,13 @@ public class QuestionController {
         return ResponseEntity.ok(questionService.findFirstQuestion("headache"));
     }
 
-    // 두통 마지막 초기 질문
+    // 두통 마지막 초기 질문 응답
     @PostMapping(value = "/headache/last-default")
     public ResponseEntity<?> getHeadacheDefaultQuestionAfter(@RequestBody @Valid HeadacheDefaultQuestionAfterRequestDto dto){
         return ResponseEntity.ok(questionService.findHeadacheDefaultQuestionAfter(dto));
     }
 
+    // 두통 결정적 진단 응답
     @PostMapping (value = "/headache/decisive")
     public ResponseEntity<?> getHeadacheDecisiveQuestion(@RequestBody @Valid HeadacheDecisiveQuestionRequestDto dto) {
         return ResponseEntity.ok(questionService.findHeadacheDecisiveQuestion(dto));
