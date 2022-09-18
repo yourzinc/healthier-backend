0313f8ba5855c12e831de855a137c9599dd95dff
offsujin <offsujin@gmail.com>
2022-09-18 20:29:21 +0700

    Fix : TimeZone 한국으로 수정

--- src/main/java/com/healthier/diagnosis/DiagnosisApplication.java
+++ src/main/java/com/healthier/diagnosis/DiagnosisApplication.java
@@ -2,9 +2,16 @@ package com.healthier.diagnosis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DiagnosisApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiagnosisApplication.class, args);
