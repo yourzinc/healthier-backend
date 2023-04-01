0f2112e20da03b6740baefff437c67f0503f7364
yourzinc <your_zinc@ewhain.net>
2023-04-01 13:38:38 +0900

    refactor: bannerImg 필드 이름 변경

--- src/main/java/com/healthier/diagnosis/dto/headache/result/HeadacheResultDto.java
+++ src/main/java/com/healthier/diagnosis/dto/headache/result/HeadacheResultDto.java
@@ -1,20 +1,23 @@
 package com.healthier.diagnosis.dto.headache.result;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class HeadacheResultDto {
     private int id;
     private String content;
-    private String bannerImg;
+    private String bannerIllustration;
 
     protected HeadacheResultDto() {}
     public HeadacheResultDto(int id, String content, String bannerImg) {
         this.id = id;
         this.content = content;
-        this.bannerImg = bannerImg;
+        this.bannerIllustration = bannerImg;
     }
 
 }
